1196f960637865307741ad176c20aa2ddf9707a0
DannyGGX <danielglanz111@gmail.com>
2024-05-15 10:35:38 +0200

    Player colour gets shown on the turn indicator
    setting Image.color makes the image invisible, so now the 2 players have separate images

--- Turn Based AI - Daniel/Assets/_Scripts/Player/PlayerTurnIndicator.cs
+++ Turn Based AI - Daniel/Assets/_Scripts/Player/PlayerTurnIndicator.cs
@@ -13,7 +13,8 @@ namespace DannyG
 		[SerializeField] private Color player2Color;
 		
 		[FormerlySerializedAs("_turnText")] [SerializeField] private TextMeshProUGUI turnText;
-		[SerializeField] private Image colorIndicator;
+		[SerializeField] private Image player1Image;
+		[SerializeField] private Image player2Image;
 		
 		private void OnEnable()
 		{
@@ -30,7 +31,19 @@ namespace DannyG
 		private void ChangeIndicatorText(PlayerId playerId)
 		{
 			turnText.text = $"Player {(int)playerId} Turn";
-			colorIndicator.color = playerId == PlayerId.Player1 ? player1Color : player2Color;
+			//colorIndicator.color = playerId == PlayerId.Player1 ? player1Color : player2Color; // this makes the image invisible
+			if (playerId == PlayerId.Player1)
+			{
+				
+				player1Image.enabled = true;
+				player2Image.enabled = false;
+				
+			}
+			else
+			{
+				player1Image.enabled = false;
+				player2Image.enabled = true;
+			}
 		}
 		
 	}
