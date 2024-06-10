db3ff328d027c313b7e9711546969aa205c87d65
DannyGGX <danielglanz111@gmail.com>
2024-06-10 10:07:27 +0200

    Added comments, and support for searching for empty tiles

--- Turn Based AI - Daniel/Assets/_Scripts/Board/LineOfPiecesOperations.cs
+++ Turn Based AI - Daniel/Assets/_Scripts/Board/LineOfPiecesOperations.cs
@@ -6,18 +6,25 @@ using UnityEngine;
 
 namespace DannyG
 {
-	
+	/// <summary>
+	/// Used to find the amount of pieces in a line in a given board state.
+	///
+	/// The win checker and the utility function of the minimax algorithm use this class.
+	/// </summary>
 	public static class LineOfPiecesOperations
 	{
 		/// <summary>
-		/// 
+		/// Used to find coordinates of a certain tile type around a center coordinate.
 		/// </summary>
-		/// <param name="centerCoordinate"></param>
-		/// <param name="boardState"></param>
-		/// <returns> List of coordinates of pieces with the same tile type as the center coordinate </returns>
-		public static List<Coordinate> GetNeighboringCoordinatesOfSameTileType(Coordinate centerCoordinate, BoardState boardState)
+		/// <param name="centerCoordinate"> The placed piece from which to get the neighboring coordinates </param>
+		/// <param name="boardState">  </param>
+		/// <param name="targetTileType"> Only coordinates with this tile type will be returned.
+		/// If null, then it will be set to the tile type of the center coordinate. </param>
+		/// <returns> List of neighboring coordinates </returns>
+		public static List<Coordinate> GetNeighboringCoordinates(Coordinate centerCoordinate, BoardState boardState, TileType? targetTileType = null)
 		{
-			TileType targetTileType = boardState.GetTileTypeAt(centerCoordinate);
+			// if targetTileType is null, then it will be set to the tile type of the center coordinate
+			targetTileType ??= boardState.GetTileTypeAt(centerCoordinate);
 			HashSet<Coordinate> possibleCoordinates = GetPossibleCoordinatesAroundCenterCoordinate();
 			var resultCoordinates = new List<Coordinate>();
 			
@@ -75,19 +82,25 @@ namespace DannyG
 		}
 
 		/// <summary>
-		/// 
+		/// Used to find the amount of tiles of a particular type in a line starting from a center coordinate.
+		/// The opposite direction is also checked.
 		/// </summary>
 		/// <param name="centerCoordinate"> The starting coordinate </param>
-		/// <param name="nextCoordinate"> The coordinate next to the center coordinate </param>
+		/// <param name="nextCoordinate"> The coordinate next to the center coordinate.
+		/// The incrementor is determined by the difference between the 2 given coordinates. </param>
 		/// <param name="boardState"></param>
-		/// <returns> Number of tiles with the same tile type as the center coordinate. returns values 2 to 4 </returns>
-		public static int GetNumberOfSameTilesInALine(Coordinate centerCoordinate, Coordinate nextCoordinate, 
+		/// <returns> Number of same tiles in a line starting from center coordinate in the direction of the next coordinate
+		/// as well as the opposite direction.
+		/// Returns values 2 to 4 </returns>
+		public static int GetNumberOfTilesInALine(Coordinate centerCoordinate, Coordinate nextCoordinate, 
 			BoardState boardState)
 		{
-			TileType targetTileType = boardState.GetTileTypeAt(centerCoordinate);
+			// The target tile type is set to the tile type of the next coordinate.
+			// This can be used for checking the amount of empty tiles around a placed piece.
+			TileType targetTileType = boardState.GetTileTypeAt(nextCoordinate);
 			Incrementor2D incrementor = new Incrementor2D(centerCoordinate, nextCoordinate);
-			int numberOfSameTiles = 1;
-			Coordinate currentCoordinate = centerCoordinate;
+			int numberOfSameTiles = 2;
+			Coordinate currentCoordinate = nextCoordinate;
 
 			CountSameTiles();
 			
@@ -114,21 +127,25 @@ namespace DannyG
 					if (currentTile == targetTileType)
 					{
 						numberOfSameTiles++;
-						continueLoop = numberOfSameTiles < 4;
+						continueLoop = numberOfSameTiles < 4; // exit the loop if enough of the same tiles are found. 4 is enough
 					}
-					else
-					{
-						return;
-					}
+					else return;
 				}
 			}
 		}
 
+		/// <summary>
+		/// Find the longest line of tiles around a center coordinate.
+		/// The target tile type is the same as the center coordinate.
+		/// </summary>
+		/// <param name="centerCoordinate"></param>
+		/// <param name="boardState"></param>
+		/// <returns> The number of coordinates in the longest line. Returns values 1 to 4. </returns>
 		public static int GetLongestLineOfTilesInArea(Coordinate centerCoordinate, BoardState boardState)
 		{
 			var result = 1;
 			const int cutOff = 4;
-			List<Coordinate> neighboringCoordinates = GetNeighboringCoordinatesOfSameTileType(centerCoordinate, boardState);
+			List<Coordinate> neighboringCoordinates = GetNeighboringCoordinates(centerCoordinate, boardState);
 			if(neighboringCoordinates.Count == 0)
 			{
 				return result;
@@ -136,7 +153,7 @@ namespace DannyG
 
 			foreach (var coordinate in neighboringCoordinates)
 			{
-				int currentLineCount = GetNumberOfSameTilesInALine(centerCoordinate, coordinate, boardState);
+				int currentLineCount = GetNumberOfTilesInALine(centerCoordinate, coordinate, boardState);
 				if (currentLineCount >= cutOff)
 				{
 					return currentLineCount;
